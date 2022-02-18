9c45bd666064519171d630563a6206a33e8bef41
adritadeb <adritad2019@gmail.com>
2022-02-18 14:18:30 +0600

    penguin commit

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log(document.getElementById('jacket-btn'));
+document.getElementById('first-button').addEventListener('click', function () {
+    document.getElementById('penguin-title').innerText = 'I AM PENGUIN';
+})
